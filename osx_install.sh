097d1d7f6aa9df4c19d929453adf4e245b3542ce
Mario Zaizar <mariozaizar@gmail.com>
2017-02-09 14:18:33 -0800

    Start using Development packages folder for development.

--- osx_install.sh
+++ osx_install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-INSTALL_PATH=$HOME/.atom/packages/language-eml-dev
-ln -siv $PWD $INSTALL_PATH
+INSTALL_PATH=$HOME/.atom/dev/packages;
+mkdir -p $INSTALL_PATH;
 
-echo "This directory got installed in $INSTALL_PATH"
+PACKAGE_PATH=$INSTALL_PATH/language-eml;
+ln -siv $PWD $INSTALL_PATH && echo "This directory got linked in $PACKAGE_PATH" || echo "Can't link this directory to $PACKAGE_PATH"
